b4f439fd0a7b08f5e30781e328ed1d32891ff154
YukiArataki <lohannabietvieira.com>
2024-06-12 18:43:21 -0300

    version9

--- restaurante/src/br/edu/up/view/PedidoView.java
+++ restaurante/src/br/edu/up/view/PedidoView.java
@@ -12,13 +12,16 @@ public class PedidoView {
     private Scanner scanner = new Scanner(System.in);
     private PedidoController pedidoController = new PedidoController();
     private ProdutoView produtoView = new ProdutoView(); // Utilizado para seleccionar productos
+    int opcao;
     do{
+         opcao = scanner.nextInt();
+        scanner.nextLine();
+
         System.out.println("=== Menu Principal ===");
         System.out.print("Escolha uma opção: ");
         System.out.println("1.Escolher pedido");
-        System.out.println("2. Deletar pedido");
-        System.out.println("3. Calcular o total");
-        System.out.println("4. Sair");
+        System.out.println("2. Calcular o total");
+        System.out.println("3. Sair");
         opcao = scanner.nextInt();
         scanner.nextLine();
 
@@ -30,6 +33,17 @@ public class PedidoView {
     public void adicionarPedido() {
         List<Produto> produtos = new ArrayList<>();
         // Lógica para permitir al usuario seleccionar productos y añadirlos a la lista de productos
+         switch (opcao){
+            case 1:
+            adicionarPedido();
+            break;
+            case 2:
+            calcularTotal();
+            break;
+            case 3:
+            System.out.println("Saindo do programa...");
+            break;
+         }
 
         double total = calcularTotal(produtos); // Implementa esta función según tu lógica de negocio
         Pedido pedido = new Pedido(produtos, total);
