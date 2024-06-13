bda2be9d0d36af71165efdd59b5a77cb69d6275c
Auto Logon <autologon@CSED.LAB>
2024-06-13 08:48:35 -0300

    Atualização Main/MainView

--- restaurante/src/br/edu/up/view/MainView.java
+++ restaurante/src/br/edu/up/view/MainView.java
@@ -1,46 +1,47 @@
 package br.edu.up.view;
 
-import br.edu.up.controller.ClienteController;
-import br.edu.up.controller.DeliveryController;
-import br.edu.up.controller.PedidoController;
-import br.edu.up.controller.ProdutoController;
 import br.edu.up.controller.ReservaController;
+import br.edu.up.dao.ReservaDAO;
 
 import java.util.Scanner;
 
 public class MainView {
-    public Scanner scanner = new Scanner(System.in);
-    public ClienteController clienteController = new ClienteController();
-    public DeliveryController deliveryController = new DeliveryController();
-    public PedidoController pedidoController = new PedidoController();
-    public ProdutoController produtoController = new ProdutoController();
-    public ReservaController reservaController = new ReservaController();
-
-    public void exibirMenu() {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int opcao;
+
+        DeliveryView deliveryView = new DeliveryView();
+        PedidoView pedidoView = new PedidoView();
+        ProdutoView produtoView = new ProdutoView();
+        ReservaView reservaView = new ReservaView();
+
+        // Adicionando instância ReservaController
+        ReservaDAO reservaDAO = new ReservaDAO();
+        ReservaController reservaController = new ReservaController(reservaDAO, reservaView);
+
         do {
-            System.out.println("=== Menu ===");
-            System.out.println("1. Cadastro de Cliente");
-            System.out.println("2. Pedido");
-            System.out.println("3. Delivery");
-            System.out.println("4. Reserva");
+            System.out.println("=== Menu Principal ===");
+            System.out.println("1. Cadastro de Produtos");
+            System.out.println("2. Realizar Reserva");
+            System.out.println("3. Fazer Pedido");
+            System.out.println("4. Solicitar Delivery");
             System.out.println("5. Sair");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
-            scanner.nextLine(); // Consumir a quebra de linha
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
-                    exibirMenuCliente();
+                    produtoView.adicionarProduto();
                     break;
                 case 2:
-                    exibirMenuPedido();
+                    reservaController.iniciar(); // Chamando o método iniciar do controller para abrir o menu de reservas
                     break;
                 case 3:
-                    exibirMenuDelivery();
+                    pedidoView.adicionarPedido();
                     break;
                 case 4:
-                    exibirMenuReserva();
+                    deliveryView.adicionarDelivery();
                     break;
                 case 5:
                     System.out.println("Saindo...");
@@ -49,21 +50,7 @@ public class MainView {
                     System.out.println("Opção inválida. Tente novamente.");
             }
         } while (opcao != 5);
-    }
 
-    private void exibirMenuCliente() {
-        // Implementar a lógica para o menu de cliente
-    }
-
-    private void exibirMenuPedido() {
-        // Implementar a lógica para o menu de pedido
-    }
-
-    private void exibirMenuDelivery() {
-        // Implementar a lógica para o menu de delivery
-    }
-
-    private void exibirMenuReserva() {
-        // Implementar a lógica para o menu de reserva
+        scanner.close();
     }
 }
